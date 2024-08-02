7bb79d9cb4c81a1c57217280661b31e16b193fae
Carla-coder <cmartezanato@yahoo.com.br>
2024-08-02 18:48:03 -0300

    Att

--- app native/MarketChecklist/App.js
+++ app native/MarketChecklist/App.js
@@ -1,4 +1,24 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Home from './src/pages/Home';
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={Home} options={{ title: 'Market Checklist' }} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/pages/Home';
@@ -9,10 +29,38 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Market Checklist' }} />
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{ 
+            headerTitle: () => (
+              <View style={styles.headerContainer}>
+                <Image 
+                  source={require('./assets/favicon.png')} 
+                  style={styles.logo} 
+                />
+              </View>
+            ),
+            headerTitleAlign: 'center',
+          }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const styles = StyleSheet.create({
+  headerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100, 
+    height: 100, 
+  },
+});
+
 export default App;
+
+
